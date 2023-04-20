62e3b06466d2e434f42fb711bf9568864a59f90a
VF2048 <valera.fokin2012@yandex.ru>
2023-04-20 14:01:44 +0300

    add HW type

--- injected/filter/filterMain.js
+++ injected/filter/filterMain.js
@@ -9,7 +9,7 @@ function dataRequest() {
     let find;
     tasks = [];
     let reg = /((RITM|INC)\d{9})|(TASK\d{8})/gm;
-    let regCMDB = /(HW\d{6})/gm;
+    let regCMDB = /(HW\d{6})|(HW\d{5})/gm;
     let task = prompt("Введите список ритмов или инцидентов. Нажмите 'ок', далее 'Применить' и обновите страницу.", "Любой текст содержащий номера заданий RITM001465684 RITM001465681");
     while ((find = reg.exec(task)) !== null)
         tasks.push({ value: find[0], typeValue: "Number" });
