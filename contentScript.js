089e99bf08f6ec309d4bca3e11f9431c6a578939
Vf2048 <valera.fokin2012@yandex.ru>
2023-10-12 19:37:57 +0300

    change config update

--- contentScript.js
+++ contentScript.js
@@ -46,26 +46,36 @@ let Scripts = {
     }
 }
 
-async function checkhashtagTreeVersion() {
-    const hashtagTreeURL = chrome.runtime.getURL("injected/configs/hashtagTree.js");
-    let version = await fetch(hashtagTreeURL)
+function getLocalFile(url, regex) {
+    const hashtagTreeURL = chrome.runtime.getURL(url);
+    return fetch(hashtagTreeURL)
         .then(responce => responce.text())
         .then(text => {
-            const regex = /const hashtagVersion = "([^"]+)"/;
+            // const regex = /const hashtagVersion = "([^"]+)"/;
             const matchs = text.match(regex);
             if (matchs && matchs.length >= 2)
                 return matchs[1];
         })
-        .catch(error => console.error(error));
+        .catch(error => console.log(`Не был найден файл ${url}`));
+}
+
+async function checkhashtagTreeVersion() {
+
 
     let netVersion = await chrome.runtime.sendMessage({ action: "fetchData", url: "file://softcloud//Library//SoftArm//hashtagTree//hashtagTree.js" });
-    if(!netVersion){
+    if (!netVersion) {
         alert("Не удалось найти hashtagTree.js на softcloud!");
         return;
     }
-    if (version !== netVersion){
+    let version = await getLocalFile("injected/configs/hashtagTree.js", /const hashtagVersion = "([^"]+)"/);
+    if (version !== netVersion) {
         chrome.runtime.sendMessage({ action: "download", url: "file://softcloud//Library//SoftArm//hashtagTree//hashtagTree.js" });
         alert("Файл hashtagTree.js устарел и требует замены.\nСкачайте данный файл и поместите его в папку расширения\ninjected/configs/hashtagTree.js");
+    }
+    let config = await getLocalFile("injected/configs/configHashtag.js", /(startLevelRitm)/);
+    if (typeof config === "undefined") {
+        chrome.runtime.sendMessage({ action: "download", url: "file://softcloud//Library//SoftArm//hashtagTree//configHashtag.js" });
+        alert("У вас отсутствует файл конфигурации!\nРазрешите скачивание и переместите его в папку расширения\ninjected/configs/configHashtag.js");
     }
 }
 
